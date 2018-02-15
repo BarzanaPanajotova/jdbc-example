8c594276c41a9fca6d3f2165b7caf72524ac43c5
barzana.panayotova <barzana.panayotova@ntb182.scalefocus.com>
2018-02-15 17:55:00 +0200

    refactor

--- jdbc-example/src/main/java/com/bdpanajoto/examples/Application.java
+++ jdbc-example/src/main/java/com/bdpanajoto/examples/Application.java
@@ -3,6 +3,7 @@ package com.bdpanajoto.examples;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.function.Consumer;
 
@@ -12,6 +13,26 @@ import com.bdpanajoto.examples.db.initialize.SetupDatabase;
 
 public class Application {
 
+	
+	private static final String Y = "y";
+	private static final String EMPTY_STRING = "";
+	private static final String DEFAULT_PORT = "1521";
+	private static final String DEFAULT_DB_NAME = "xe";
+	private static final String DEFAULT_SERVER = "localhost";
+	private static final String DEFAULT_DRIVER = "thin";
+	private static final String ENTER_PASSWORD_MSG = "Enter password:";
+	private static final String ENTER_USERNAME_MSG = "Enter username:";
+	private static final String ENTER_PORT_NO_MSG = "Enter port No(default:1521):";
+	private static final String ENTER_DB_NAME_MSG = "Enter db name(default:xe):";
+	private static final String ENTER_SERVER_NAME_MSG = "Enter server name/address(default:localhost):";
+	private static final String ENTER_DRIVER_MSG = "Enter driver(default:thin):";
+	private static final String NOT_A_VALID_OPTION_MSG = "{0} is not a valid option!\n";
+	private static final String VALID_PROGRAM_OPTIONS_MSG = "Valid program options are:\n";
+	private static final String THERE_IS_NO_CONNECTION_MSG = "There is no connection to a db!";
+	private static final String NOT_EXECUTING_INIT_MSG = "Not executing init!";
+	private static final String ARE_YOU_SURE_MSG = "Are you sure you want to initalize(existing objects in the db schema will be dropped!)? Y/N";
+	
+
 	public static void main(String[] args) throws IOException {
 		init();
 	}
@@ -20,6 +41,17 @@ public class Application {
 		printOptions();
 
 		handleInput();
+	}
+
+	private static void printOptions() {
+		System.out.println(VALID_PROGRAM_OPTIONS_MSG);
+		Consumer<ProgramOption> printOptionInfo = opt -> System.out.println(opt.getName() + "   " + opt.getText());
+
+		Arrays.asList(ProgramOption.values()).stream()
+
+				.filter(x -> !x.equals(ProgramOption.INVALID))
+
+				.forEach(printOptionInfo);
 	}
 
 	private static void handleInput() throws IOException {
@@ -44,23 +76,12 @@ public class Application {
 					break;
 				default:
 					if (!optionStr.isEmpty()) {
-						System.out.println(optionStr + " is not a valid option!\n");
+						System.out.println(MessageFormat.format(NOT_A_VALID_OPTION_MSG, optionStr));
 						printOptions();
 					}
 				}
 			}
 		}
-	}
-
-	private static void printOptions() {
-		System.out.println("Valid program options are:\n");
-		Consumer<ProgramOption> printOptionInfo = opt -> System.out.println(opt.getName() + "   " + opt.getText());
-
-		Arrays.asList(ProgramOption.values()).stream()
-
-				.filter(x -> !x.equals(ProgramOption.INVALID))
-
-				.forEach(printOptionInfo);
 	}
 
 	private static void exit() {
@@ -69,20 +90,19 @@ public class Application {
 	}
 
 	private static DatabaseInterface initializeDBConnection(BufferedReader reader) throws IOException {
-
-		String driver = parseInput(reader, "Enter driver(default:thin):", "thin");
-		String serverName = parseInput(reader, "Enter server name/address(default:localhost):", "localhost");
-		String databaseName = parseInput(reader, "Enter db name(default:xe):", "xe");
-		String portString = parseInput(reader, "Enter port No(default:1521):", "1521");
+		String driver = parseDBConnectionInput(reader, ENTER_DRIVER_MSG, DEFAULT_DRIVER);
+		String serverName = parseDBConnectionInput(reader, ENTER_SERVER_NAME_MSG, DEFAULT_SERVER);
+		String databaseName = parseDBConnectionInput(reader, ENTER_DB_NAME_MSG, DEFAULT_DB_NAME);
+		String portString = parseDBConnectionInput(reader, ENTER_PORT_NO_MSG, DEFAULT_PORT);
 		int port = Integer.parseInt(portString);
-		String username = parseInput(reader, "Enter username:", "");
-		char[] password = parseInput(reader, "Enter password:", "").toCharArray();
+		String username = parseDBConnectionInput(reader, ENTER_USERNAME_MSG, EMPTY_STRING);
+		char[] password = parseDBConnectionInput(reader, ENTER_PASSWORD_MSG, EMPTY_STRING).toCharArray();
 
 		return DatabaseInterfaces.getSimpleDatabaseInterface(driver, serverName, databaseName, port, username,
 				password);
 	}
 
-	private static String parseInput(BufferedReader reader, String queryString, String defaultValue)
+	private static String parseDBConnectionInput(BufferedReader reader, String queryString, String defaultValue)
 			throws IOException {
 		System.out.println(queryString);
 		String input = reader.readLine();
@@ -91,15 +111,14 @@ public class Application {
 
 	private static void initializeDBObjects(BufferedReader reader, DatabaseInterface dbInterface) throws IOException {
 		if (dbInterface != null) {
-			System.out.println(
-					"Are you sure you want to initalize(existing objects in the db schema will be dropped!)? Y/N");
-			if ("y".equalsIgnoreCase(reader.readLine())) {
+			System.out.println(ARE_YOU_SURE_MSG);
+			if (Y.equalsIgnoreCase(reader.readLine())) {
 				SetupDatabase.init(dbInterface);
 			} else {
-				System.out.println("Not executing init!");
+				System.out.println(NOT_EXECUTING_INIT_MSG);
 			}
 		} else {
-			System.out.println("There is no connection to a db!");
+			System.out.println(THERE_IS_NO_CONNECTION_MSG);
 		}
 	}
 
@@ -131,7 +150,10 @@ public class Application {
 		}
 
 		public static ProgramOption resolve(String str) {
-			return Arrays.asList(ProgramOption.values()).stream().filter(x -> x.name.equalsIgnoreCase(str)).findFirst()
+			return Arrays.asList(ProgramOption.values()).stream()
+
+					.filter(x -> x.name.equalsIgnoreCase(str)).findFirst()
+
 					.orElse(INVALID);
 		}
 	}
